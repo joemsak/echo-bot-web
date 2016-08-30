df4eec0162a99dfff9ffc8b3cd0c681a051222b8
Joe Sak <joe@joesak.com>
2016-08-30 16:17:35 -0500

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://echo-chmbr.herokuapp.com/posts", { mode: 'no-cors' })
+    .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      return this.setState({ posts: json.data });
+    }.bind(this));
+  }
+
+  displayPosts() {
+    if(this.state.posts.length === 0) {
+      return "No posts.";
+    } else {
+      return this.state.posts[0].url;
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.displayPosts()}
+      </div>
+    );
+  }
+}
+
+export default App;
