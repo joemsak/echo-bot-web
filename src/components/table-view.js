062aa804a26aac935956af014c29aa97b621ce58
Joe Sak <joe@joesak.com>
2016-09-01 19:38:32 -0500

    Improve table-view with new API, remove Header don't need that

--- src/components/table-view.js
+++ src/components/table-view.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 import get from '../utils/api';
-import Headers from '../utils/headers';
 
 class TableView extends Component {
   constructor(props) {
     super(props)
     this.state = {
       items: [],
-      headers: Headers(this.resource()),
+      name: "",
       pathname: (props.location || {}).pathname || "/posts",
     }
   }
@@ -20,71 +19,80 @@ class TableView extends Component {
 
   componentDidMount() {
     get(this.state.pathname, (json) => {
-      return this.setState({ items: [].concat(json.data) })
+      return this.setState({
+        items: [].concat(json.data),
+        name: json.name,
+      })
     });
   }
 
   componentWillReceiveProps(nextProps) {
     this.setState({
       pathname: nextProps.location.pathname,
-      headers: Headers(this.resource(nextProps)),
     });
 
     get(nextProps.location.pathname, (json) => {
-      return this.setState({ items: [].concat(json.data) })
+      return this.setState({
+        items: [].concat(json.data),
+        name: json.name,
+      })
     });
   }
 
-  tableCell(item, header) {
-    let content;
-
-    if(header.match(/\w+_id$/)) {
-      const linkRoot = header.replace('_id', 's'),
-            linkName = linkRoot.replace(/^[a-z]/, (x) => { return x.toUpperCase() })
-                               .replace(/s$/, '');
-
-      content = <Link to={`/${linkRoot}/${item[header]}`}>
-                  {`${linkName} #${item[header]}`}
-                </Link>;
-    } else {
-      content = item[header];
-    };
-
-    return <td key={`item-${item['id']}-${header}`}>{content}</td>;
+  tableHeaders() {
+    return Object.keys(this.state.items[0]).map((k) => {
+      return (
+        <th key={`header-${k}`}>
+          {this.state.items[0][k].display_name}
+        </th>
+      );
+    });
   }
 
   tableRows() {
-    if(this.state.items.length > 0) {
-      return this.state.items.map((item) => {
-        return (
-          <tr key={`item-${item['id']}`}>
-            {this.state.headers.map((header) => { return this.tableCell(item, header) })}
-          </tr>
-        );
-      });
+    return this.state.items.map((item) => {
+      return (
+        <tr key={`item-${item.id.value}`}>
+          {Object.keys(item).map((k) => {
+            return (
+              <td key={`item-col-${item.id.value}-${k}`}>
+                {this.tableCell(item[k])}
+              </td>
+            );
+          })}
+        </tr>
+      );
+    });
+  }
+
+  tableCell(attribute) {
+    if (attribute.value) {
+      return attribute.value;
     } else {
-      return <tr><td colSpan={this.state.headers.length}>Loading...</td></tr>;
+      return <Link to={attribute.link.href}>{attribute.link.text}</Link>;
     }
   }
 
   render() {
-    return (
-      <table className="table table-striped table-hover">
-        <caption>{this.resource().replace(/^[a-z]/, (x) => { return x.toUpperCase() })}</caption>
+    if(this.state.items.length === 0) {
+      return <p>Loading...</p>;
+    } else {
+      return (
+        <table className="table table-striped table-hover">
+          <caption>{this.state.name}</caption>
 
-        <thead>
-          <tr>
-            {this.state.headers.map((header) => {
-              return <th key={`table-header-${header}`}>{header}</th>;
-            })}
-          </tr>
-        </thead>
+          <thead>
+            <tr>
+              {this.tableHeaders()}
+            </tr>
+          </thead>
 
-        <tbody>
-          {this.tableRows()}
-        </tbody>
-      </table>
-    );
+          <tbody>
+              {this.tableRows()}
+          </tbody>
+        </table>
+      );
+    }
   }
 };
 
